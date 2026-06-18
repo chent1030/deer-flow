bf8161cf4a7a2655da5747038224348ba747ade6
chent1030 <chent1030@hotmail.com>
2026-06-18 11:51:20 +0800

    非强制检查

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,6 +16,12 @@ const withNextra = nextra({});
 
 /** @type {import("next").NextConfig} */
 const config = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output:
     process.env.NEXT_CONFIG_BUILD_OUTPUT === "standalone"
       ? "standalone"
